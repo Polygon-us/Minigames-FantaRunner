dc06937fc08c0d5f1082737335a5e579ab47ffe4
Juaner_ <juan.ramirez@polygon.us>
2025-02-12 15:56:57 -0500

    added firebase sdk for fast testing

--- Assets/Scripts/FirebaseCore/FirebaseConnection.cs
+++ Assets/Scripts/FirebaseCore/FirebaseConnection.cs
@@ -1,7 +1,9 @@
-using FirebaseWebGL.Scripts.FirebaseBridge;
+using Firebase.Extensions;
 using FirebaseCore.DTOs;
+using Firebase.Database;
 using Newtonsoft.Json;
 using UnityEngine;
+using Firebase;
 using System;
 
 namespace FirebaseCore
@@ -12,6 +14,7 @@ namespace FirebaseCore
 
         public static Action<UserInputDto> OnUserInput;
 
+#if !UNITY_EDITOR
         private void Start()
         {
             ListenToDatabaseChanges();
@@ -20,7 +23,7 @@ namespace FirebaseCore
         public void ListenToDatabaseChanges()
         {
 #if !UNITY_EDITOR
-            FirebaseDatabase.ListenForValueChanged(Room, gameObject.name, nameof(HandleValueChanged), nameof(HandleError));
+            FirebaseDatabase.ListenForChildChanged(Room, gameObject.name, nameof(HandleValueChanged), nameof(HandleError));
 #endif
         }
 
@@ -30,7 +33,7 @@ namespace FirebaseCore
 
             OnUserInput?.Invoke(inputDto);
         }
-        
+
         private void HandleError(string error)
         {
             Debug.LogError(error);
@@ -38,14 +41,48 @@ namespace FirebaseCore
 
         private void OnDisable()
         {
-#if !UNITY_EDITOR
-            FirebaseDatabase.StopListeningForValueChanged(Room, gameObject.name, nameof(HandleValueChanged), nameof(HandleError));
+#if UNITY_EDITOR
+            FirebaseDatabase.StopListeningForChildChanged(Room, gameObject.name, nameof(HandleValueChanged),
+                nameof(HandleError));
 #endif
         }
-        
+
         private static T ConvertTo<T>(string obj) where T : class
         {
             return JsonConvert.DeserializeObject<T>(obj);
         }
+#else
+        DatabaseReference reference;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(_ => {
+                reference = FirebaseDatabase.DefaultInstance.GetReference(Room);
+
+                // Listening for changes to child properties of Room
+                reference.ChildChanged += HandleChildChanged;
+            });
+        }
+
+        // Handle child changes under the Room node
+        private void HandleChildChanged(object sender, ChildChangedEventArgs e)
+        {
+            if (e.DatabaseError != null)
+            {
+                Debug.LogError("Error: " + e.DatabaseError.Message);
+                return;
+            }
+
+            // Log the changed child key and its new value
+            Debug.Log("Child changed: " + e.Snapshot.Key + " -> " + e.Snapshot.Value);
+        }
+
+        private void OnDisable()
+        {
+            reference.ChildChanged -= HandleChildChanged;
+        }
     }
-}
+
+#endif
+    }
