8e99e7d720fc7031cd67a135454f4a965c408d38
Juaner <juan.ramirez@polygon.us>
2025-02-18 10:04:49 -0500

    fix: added compiler directive

--- Assets/Scripts/FirebaseCore/FirebaseConnection.cs
+++ Assets/Scripts/FirebaseCore/FirebaseConnection.cs
@@ -13,6 +13,8 @@ namespace FirebaseCore
 
         public static Action<int> OnMovementInput;
         public static Action OnSubmitInput;
+
+#if !UNITY_EDITOR
         private void Start()
         {
             ListenToDatabaseChanges();
@@ -53,5 +55,6 @@ namespace FirebaseCore
         {
             return JsonConvert.DeserializeObject<T>(obj);
         }
+#endif
     }
 }
